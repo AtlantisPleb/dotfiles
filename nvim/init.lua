6c7192efcb93e1afe617081e994c91e56d96d577
Christopher David <chris@arcadelabs.co>
2024-02-01 18:33:45 -0600

    restore mostly-jess nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+--
+--                            ⢸⣦⡈⠻⣿⣿⣿⣶⣄
+--                            ⢸⣿⣿⣦⡈⠻⣿⣿⣿⣷⣄
+--                      ⣀⣀⣀⣀⣀⣀⣼⣿⣿⣿⣿ ⠈⠻⣿⣿⣿⣷⣄
+--                      ⠈⠻⣿⣿⣿⣿⣿⡿⠿⠛⠁   ⠈⠻⢿⣿⣿⣷⣄
+--
+-- Personal Neovim configuration of Jess Archer <jess@jessarcher.com>
+
+require('user.options')
+require('user.keymaps')
+require('user.misc')
+require('user.plugins')
+
+-- vim: nospell
